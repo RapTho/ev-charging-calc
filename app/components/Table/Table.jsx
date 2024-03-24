5053c7a46a10cdcc370584c4b1af43f29bb2171d
Raphael Tholl <raphael.tholl@protonmail.com>
2024-03-24 19:44:22 +0100

    wip

--- app/components/Table/Table.jsx
+++ app/components/Table/Table.jsx
@@ -1,31 +1,40 @@
 import getTableData from "@/lib/getTableData";
 
-import styles from "./Table.module.scss";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-export default async function Table() {
+export default async function RecentCharges() {
   const start = process.env.START || Date.parse("February 22, 2024 14:00:00");
   const end = process.env.END || Date.now();
 
   const data = await getTableData(start, end);
 
   return (
-    <table className={styles.table}>
-      <thead className={styles.header}>
-        <tr>
-          <th className={styles.dateCol}>Date</th>
-          <th className={styles.kWhCol}>Consumption</th>
-        </tr>
-      </thead>
-      <tbody>
+    <Table>
+      <TableCaption>A list of your recent charges.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Date</TableHead>
+          <TableHead className="text-right">Amount [kWh]</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
         {data.map((elem, index) => {
           return (
-            <tr key={index} className={styles.row}>
-              <td className={styles.dateCol}>{elem.DateTime}</td>
-              <td className={styles.kWhCol}>{elem.Consumption} kWh</td>
-            </tr>
+            <TableRow key={index}>
+              <TableCell>{elem.DateTime}</TableCell>
+              <TableCell className="text-right">{elem.Consumption}</TableCell>
+            </TableRow>
           );
         })}
-      </tbody>
-    </table>
+      </TableBody>
+    </Table>
   );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import "./globals.scss";
+import "./globals.css";
 
 import Logo from "./components/Logo";
 import Header from "./components/Header";
