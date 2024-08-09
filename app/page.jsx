0e36f52288c73567ccf4922d408966c3e3564009
Raphael Tholl <raphael.tholl@protonmail.com>
2024-08-09 15:28:16 +0200

    remove hidden tailwind class to enable mobile view

--- app/page.jsx
+++ app/page.jsx
@@ -46,7 +46,7 @@ export default function DashboardPage() {
   }, [start, end]);
 
   return (
-    <div className="hidden flex-col md:flex">
+    <div className="flex-col md:flex">
       <div className="flex-1 space-y-4 p-4 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
